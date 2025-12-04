ef3cb3f843e8afd89ba8e760066c652fd75d99ea
Austin Townsend <130945920+Pancakeshack@users.noreply.github.com>
2025-12-03 21:05:09 -0700

    add rainbow wheel

--- sketches/blink/blink.ino
+++ sketches/blink/blink.ino
@@ -1,13 +1,47 @@
-#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+// Define the pin and number of LEDs
+#define LED_PIN 6
+#define LED_COUNT 30
+#define MAX_BRIGHTNESS 20
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+uint32_t Wheel(byte WheelPos)
+{
+    WheelPos = 255 - WheelPos;
+    if (WheelPos < 85)
+    {
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    }
+    else if (WheelPos < 170)
+    {
+        WheelPos -= 85;
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    }
+    else
+    {
+        WheelPos -= 170;
+        return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    }
+}
 
 void setup()
 {
-    pinMode(13, OUTPUT);
+    strip.begin();
+    strip.show(); // Turn off all LEDs
+    strip.setBrightness(MAX_BRIGHTNESS);
 }
+
 void loop()
 {
-    digitalWrite(13, HIGH);
-    delay(500);
-    digitalWrite(13, LOW);
-    delay(500);
+    for (int j = 0; j < 256; j++)
+    {
+        for (int i = 0; i < strip.numPixels(); i++)
+        {
+            strip.setPixelColor(i, Wheel((i + j) & 255));
+        }
+        strip.show();
+        delay(50);
+    }
 }
